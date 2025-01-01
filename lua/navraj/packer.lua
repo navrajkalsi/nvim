4cba8eeaf1e6c73573a91c30f5f5a6328c00d276
Navraj Singh Kalsi <navrajkalsi@icloud.com>
2024-12-31 20:56:13 -0500

    changes to config

--- lua/navraj/packer.lua
+++ lua/navraj/packer.lua
@@ -28,13 +28,13 @@ return require('packer').startup(function(use)
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 
-	use( {
+	use {
 		'rose-pine/neovim',
 		as = 'rose-pine',
 		config = function()
 			vim.cmd('colorscheme rose-pine')
 		end
-	})
+	}
 
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 

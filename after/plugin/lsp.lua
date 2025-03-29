53842d982dabc170f91def5a68e7ace67944fa93
Navraj Singh Kalsi <navrajkalsi@icloud.com>
2025-03-29 03:48:37 -0400

    Files

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,15 +10,15 @@ lspconfig.jdtls.setup {
 }
 
 lspconfig.lua_ls.setup({
-    capabilities = capabilities,  -- Preserve existing capabilities
+    capabilities = capabilities, -- Preserve existing capabilities
     settings = {
         Lua = {
             diagnostics = {
-                globals = { 'vim' },  -- Tell LSP that 'vim' is a global variable
+                globals = { 'vim' }, -- Tell LSP that 'vim' is a global variable
             },
             workspace = {
                 library = vim.api.nvim_get_runtime_file("", true), -- Make LSP aware of Neovim runtime files
-                checkThirdParty = false, -- Prevent annoying third-party module warnings
+                checkThirdParty = false,                           -- Prevent annoying third-party module warnings
             },
             telemetry = {
                 enable = false, -- Disable telemetry to improve performance
@@ -36,5 +36,13 @@ lspconfig.cssls.setup {
 }
 
 lspconfig.ts_ls.setup {
+    capabilities = capabilities
+}
+
+lspconfig.clangd.setup {
+    capabilities = capabilities
+}
+
+lspconfig.jsonls.setup {
     capabilities = capabilities
 }

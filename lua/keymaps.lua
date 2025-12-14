2828ae4545733f27444f760f03ef9c5f80b17ebb
Navraj Kalsi <navrajkalsi@icloud.com>
2025-12-14 11:56:07 -0500

    first

---
+++ lua/keymaps.lua
@@ -0,0 +1,40 @@
+local opts = { noremap = true, silent = true }
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear highlights when pressing Esc' })
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+vim.keymap.set('n', '<leader>pv', ':Telescope file_browser path=%:p:h select_buffer=false<CR>', opts)
+vim.keymap.set("n", "<C-c>", ":q<CR>", opts)
+
+-- Copying & pasing
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], opts)
+vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]], opts)
+
+-- Line moving while in visual
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
+
+-- Centering after moving
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
+vim.keymap.set("n", "n", "nzzzv", opts)
+vim.keymap.set("n", "N", "Nzzzv", opts)
+
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
---
+++ lua/options.lua
@@ -0,0 +1,54 @@
+vim.g.have_nerd_font = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.showmode = false
+
+vim.opt.fillchars = {
+  eob = " "
+}
+vim.opt.listchars = {
+  tab = "→ ",
+  lead = "·",
+  trail = "·",
+  nbsp = "␣"
+}
+
+vim.opt.breakindent = true
+
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Always show a sign column. This prevents text from shifting when signs appear or disappear.
+vim.opt.signcolumn = 'yes'
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Show invisible chars
+vim.opt.list = true
+
+vim.opt.cursorline = true
+
+vim.opt.scrolloff = 10
+
+vim.opt.confirm = true
+
+-- All windows are of equal size
+vim.opt.equalalways = true
+
+vim.opt.cmdheight = 0
+
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+vim.opt.smoothscroll = true
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+vim.opt.textwidth = 100
